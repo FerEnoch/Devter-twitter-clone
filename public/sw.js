4188786cbb6bd9f1780c8eb4f0bd10806ab97897
Fer Enoch <ferenochsoames@gmail.com>
2023-06-13 17:50:54 -0300

    bug-fixing ignoring service worker files

--- public/sw.js
+++ public/sw.js
@@ -13,68 +13,67 @@
 
 // If the loader is already loaded, just stop.
 if (!self.define) {
-  const registry = {}
+  let registry = {};
 
   // Used for `eval` and `importScripts` where we can't get script URL by other means.
   // In both cases, it's safe to use a global var because those functions are synchronous.
-  let nextDefineUri
+  let nextDefineUri;
 
   const singleRequire = (uri, parentUri) => {
-    uri = new URL(uri + '.js', parentUri).href
+    uri = new URL(uri + ".js", parentUri).href;
     return registry[uri] || (
-
-      new Promise(resolve => {
-        if ('document' in self) {
-          const script = document.createElement('script')
-          script.src = uri
-          script.onload = resolve
-          document.head.appendChild(script)
-        } else {
-          nextDefineUri = uri
-          importScripts(uri)
-          resolve()
-        }
-      })
-
-        .then(() => {
-          const promise = registry[uri]
-          if (!promise) {
-            throw new Error(`Module ${uri} didn’t register its module`)
+      
+        new Promise(resolve => {
+          if ("document" in self) {
+            const script = document.createElement("script");
+            script.src = uri;
+            script.onload = resolve;
+            document.head.appendChild(script);
+          } else {
+            nextDefineUri = uri;
+            importScripts(uri);
+            resolve();
           }
-          return promise
         })
-    )
-  }
+      
+      .then(() => {
+        let promise = registry[uri];
+        if (!promise) {
+          throw new Error(`Module ${uri} didn’t register its module`);
+        }
+        return promise;
+      })
+    );
+  };
 
   self.define = (depsNames, factory) => {
-    const uri = nextDefineUri || ('document' in self ? document.currentScript.src : '') || location.href
+    const uri = nextDefineUri || ("document" in self ? document.currentScript.src : "") || location.href;
     if (registry[uri]) {
       // Module is already loading or loaded.
-      return
+      return;
     }
-    const exports = {}
-    const require = depUri => singleRequire(depUri, uri)
+    let exports = {};
+    const require = depUri => singleRequire(depUri, uri);
     const specialDeps = {
       module: { uri },
       exports,
       require
-    }
+    };
     registry[uri] = Promise.all(depsNames.map(
       depName => specialDeps[depName] || require(depName)
     )).then(deps => {
-      factory(...deps)
-      return exports
-    })
-  }
+      factory(...deps);
+      return exports;
+    });
+  };
 }
-define(['./workbox-8f0e986c'], function (workbox) {
-  'use strict'
+define(['./workbox-8f0e986c'], (function (workbox) { 'use strict';
 
-  importScripts()
-  self.skipWaiting()
-  workbox.clientsClaim()
-  workbox.registerRoute('/', new workbox.NetworkFirst({
-    cacheName: 'start-url',
+  importScripts();
+  self.skipWaiting();
+  workbox.clientsClaim();
+  workbox.registerRoute("/", new workbox.NetworkFirst({
+    "cacheName": "start-url",
     plugins: [{
       cacheWillUpdate: async ({
         request,
@@ -87,15 +86,16 @@ define(['./workbox-8f0e986c'], function (workbox) {
             status: 200,
             statusText: 'OK',
             headers: response.headers
-          })
+          });
         }
-        return response
+        return response;
       }
     }]
-  }), 'GET')
+  }), 'GET');
   workbox.registerRoute(/.*/i, new workbox.NetworkOnly({
-    cacheName: 'dev',
+    "cacheName": "dev",
     plugins: []
-  }), 'GET')
-})
-// # sourceMappingURL=sw.js.map
+  }), 'GET');
+
+}));
+//# sourceMappingURL=sw.js.map
